99d2b672d2a7bc17316428c5b19a795f9b156775
Yoanna Popova <yoanna.dp@gmail.com>
2023-05-11 13:12:14 +0300

    Create render function and add delete buttons.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const calculator = document.getElementById('calculator');
 let display = document.getElementById('screen');
+
+function render() {
+    const cBtn = createButton('button', 'C', 'symbol');
+    cBtn.addEventListener('click', wipe);
+
+    const delBtn = createButton('button', 'DEL', 'symbol');
+    delBtn.addEventListener('click', wipe);
+}
+
+render();
